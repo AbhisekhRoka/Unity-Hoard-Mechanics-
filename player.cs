6a8c753d301f142b7915cf943678e46bab3368d3
Abhisekh Roka <39240620+AbhisekhRoka@users.noreply.github.com>
2026-03-12 15:00:49 -0400

    Add new systems for points and projectile size upgrades
    Add new systems for points and projectile size upgrades

--- player.cs
+++ player.cs
@@ -1,23 +1,26 @@
-// Manages Player Behaviour
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Unity.VisualScripting;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
 
-    // Player Object
+    // Player Object To Chase
     public GameObject Player;
 
     // Object to be duplicated/spawned
     public GameObject Projectile;
 
+    public TextMeshProUGUI pointsText;
+
     public float moveSpeed = 5f; 
 
     void Update()
     {
+        // Average player movement
         float horizontalInput = Input.GetAxis("Horizontal"); 
         float verticalInput = Input.GetAxis("Vertical"); 
-
 
         Vector3 moveDirection = new Vector3(horizontalInput, 0f, verticalInput);
         
@@ -26,25 +29,33 @@ public class PlayerMovement : MonoBehaviour
             moveDirection.Normalize();
         }
 
-        
         transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
 
 
-
+        // Get mouse for aim
         Mouse mouse = Mouse.current;
 
-        // Firing Mechanics
+        // Shoot bullets and work on upgrades
         if(mouse.leftButton.wasPressedThisFrame)
         {
+            // Make a copy of projectile object
             GameObject copy = Instantiate(Projectile, Player.transform.position, Player.transform.rotation);
             copy.transform.LookAt(Player.transform.position);
 
             copy.transform.position += transform.forward * Time.deltaTime * moveSpeed;
+
+            // Set size of projectile object from Variable component "size" in player object
+            float size = System.Convert.ToSingle(Variables.Object(Player).Get("size"));
+            copy.transform.localScale = new Vector3(size, size, size);
+
+            // Add script forwardBoom responsible for projectile movement and detonation to projectile
             copy.AddComponent<forwardBoom>();
         }
+        
+        // Update player points
+        pointsText.text =  ((int)Variables.Object(Player).Get("points")).ToString();
     }
 
 
 }
-
 
