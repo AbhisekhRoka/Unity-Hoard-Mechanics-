717a9a1c4a6819a2c3336f629b7dde0967ec0856
Abhisekh Roka <39240620+AbhisekhRoka@users.noreply.github.com>
2026-03-12 15:01:11 -0400

    Refactor forwardBoom for player scoring and cleanup
    Updated projectile mechanics to include player interaction and point scoring.

--- forwardBoom.cs
+++ forwardBoom.cs
@@ -1,51 +1,50 @@
-// Manages Projectile Mechanics
 using UnityEngine;
 using System.Collections; 
+using Unity.VisualScripting;
 
 
 public class forwardBoom : MonoBehaviour
 {
 
+    GameObject playerObject;
+
+    public bool des = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerObject = GameObject.FindWithTag("Player");
+        print("boom " + playerObject);
     }
 
+    // Update is called once per frame
     void Update()
     {
-        // Moves the projectile forward
         transform.position += transform.forward * Time.deltaTime * 5;
-        // Gives the projectil e2 sec lifespan or when it hits something
+
         StartCoroutine(ExplodeSoon()); 
+        
+    
     }
 
-    // Destroys the bullet in 2 secs or if it hits an "Enemy" tag
+    // Explode after 2 seconds
     IEnumerator ExplodeSoon()
     {
         yield return new WaitForSeconds(2);
-        Explode();
         Destroy(gameObject);
     }
 
-
-    // This is for whatever particle system you use for the explosion
-    void Explode() {
-        var exp = GetComponent<ParticleSystem>();
-        exp.Play();
-    }
-
     void  OnCollisionEnter (Collision collision) {
-        print(collision.gameObject.tag); 
-        
+        // Print for debug
+        print(collision.gameObject.tag);
+
+        // if collision with Enemy, set points in Player Variables to +1 and destroy projectile as well as Enemy
         if (collision.gameObject.tag == "Enemy" ) {
             print ("MUAHAHHAA SHOT DOWN");
             collision.gameObject.SetActive(false);
+            Variables.Object(playerObject).Set("points", (int)Variables.Object(playerObject).Get("points") + 1);
             Destroy(gameObject);
         }
         
     }
 
 }
-
-
-
